7859d8dfc4181dca1e88f41fb7af7c5acb9e9bbd
Nik Todorov <ntodorov@gmail.com>
2025-07-12 17:30:47 -0600

    clean up after the test

--- examples/case2-internal-service/test.sh
+++ examples/case2-internal-service/test.sh
@@ -13,6 +13,8 @@ npx dotenv-transformer gen -e dev -df ./examples/case2-internal-service  -s data
 # Compare the content of the files
 if diff -r ./examples/case2-internal-service/generated ./examples/case2-internal-service/expected -x ".keepme"; then
     echo "Files are identical"
+    echo "Removing generated files"
+    find ./examples/case2-internal-service/generated -type f ! -name ".keepme" -delete
     exit 0
 else
     echo "Files are different"
