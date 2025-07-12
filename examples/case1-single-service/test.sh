9d424d942fd1f021f5d1bfa144ef44acfcaa88ec
Nik Todorov <ntodorov@gmail.com>
2025-07-12 17:25:54 -0600

    improving more the integration tests

--- examples/case1-single-service/test.sh
+++ examples/case1-single-service/test.sh
@@ -15,6 +15,8 @@ npx dotenv-transformer gen -e dev -df ./examples/case1-single-service  -s data-p
 # Compare the content of the files
 if diff -r ./examples/case1-single-service/generated ./examples/case1-single-service/expected -x ".keepme"; then
     echo "Files are identical"
+    echo "Removing generated files"
+    find ./examples/case1-single-service/generated -type f ! -name ".keepme" -delete
     exit 0
 else
     echo "Files are different"
--- examples/case2-internal-service/test.sh
+++ examples/case2-internal-service/test.sh
@@ -5,8 +5,18 @@
 cd ..
 cd ..
 
+# run the script to generate the files
+npx dotenv-transformer gen -e dev -df ./examples/case2-internal-service  -s data-publisher  -d ./examples/case2-internal-service/generated -kv Test1KV -skv
 
-# run the script to update the files
-npx dotenv-transformer gen -e dev -df ./examples/case2-internal-service  -s data-publisher  -d ./examples/case2-internal-service/expected -kv Test1KV -skv
+# compare the files content
+
+# Compare the content of the files
+if diff -r ./examples/case2-internal-service/generated ./examples/case2-internal-service/expected -x ".keepme"; then
+    echo "Files are identical"
+    exit 0
+else
+    echo "Files are different"
+    exit 1
+fi
 
 
